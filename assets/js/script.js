75e509dceb4a22d6b5c6266db2d81a96d9b4a24d
Anderson Lucena <alucena933@gmail.com>
2023-09-27 13:48:47 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,7 @@ openMenuButton.addEventListener('click', function () {
 
 
 // Adicione um ouvinte de evento para o botão "Contatos de Emergência"
-document.getElementById('mostrarContatosBtn').addEventListener('click', function () {
-  var listaContatos = document.getElementById('lista-contatos');
-  if (listaContatos.style.display === 'none' || listaContatos.style.display === '') {
-    listaContatos.style.display = 'block';
-  } else {
-    listaContatos.style.display = 'none';
-  }
-});
+
 
 
 
--- service-worker.js
+++ service-worker.js
@@ -19,31 +19,31 @@ self.addEventListener('install', event => {
   );
 });
 self.addEventListener('message', function (event) {
-    if (event.data.action === 'skipWaiting') {
-      self.skipWaiting();
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
     }
-  });
-  
-  self.addEventListener('fetch', function (event) {
-    //Atualizacao internet
-    event.respondWith(async function () {
-      try {
-        return await fetch(event.request);
-      } catch (err) {
-        return caches.match(event.request);
+  }());
+
+  //Atualizacao cache
+  /*event.respondWith(
+  caches.match(event.request)
+    .then(function (response) {
+      if (response) {
+        return response;
       }
-    }());
+      return fetch(event.request);
+    })
+   );*/
 
-    //Atualizacao cache
-    /*event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-     );*/
+});
 
-  });
-  
