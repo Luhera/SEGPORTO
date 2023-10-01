6eed0fafa60e62af6552a922d57189ea715b8c50
Anderson Lucena <alucena933@gmail.com>
2023-10-01 17:27:53 -0300

    alteração do formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,40 @@
 
-function acessarCamera() {
-  var captureInput = document.getElementById('captureInput');
-  if (captureInput) {
-    captureInput.click();
-    captureInput.addEventListener('change', function (event) {
-      var imageFile = event.target.files[0];
-      if (imageFile) {
-        var imageElement = document.createElement('img');
-        imageElement.src = URL.createObjectURL(imageFile);
-        var imagemCapturada = document.getElementById('imagemCapturada');
-        imagemCapturada.innerHTML = ''; // Limpa qualquer imagem anterior
-        imagemCapturada.appendChild(imageElement);
+// ... (outro código anterior)
 
-        // Adicione um estilo máximo de largura para a imagem
-        imageElement.style.maxWidth = '100%';
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('form');
+  const captureInput = document.getElementById('captureInput');
+  const tirarFotoButton = document.getElementById('tirarFotoButton');
+  const imagemCapturada = document.getElementById('imagemCapturada');
+
+  // Evita que o formulário seja enviado quando o botão "Tirar foto do Incidente" for clicado.
+  tirarFotoButton.addEventListener('click', function (event) {
+      event.preventDefault(); // Evita o envio do formulário
+      acessarCamera();
+  });
+
+  // Função para acessar a câmera e exibir a imagem capturada.
+  function acessarCamera() {
+      if (captureInput) {
+          captureInput.click();
+          captureInput.addEventListener('change', function (event) {
+              var imageFile = event.target.files[0];
+              if (imageFile) {
+                  var imageElement = document.createElement('img');
+                  imageElement.src = URL.createObjectURL(imageFile);
+                  imagemCapturada.innerHTML = ''; // Limpa qualquer imagem anterior
+                  imagemCapturada.appendChild(imageElement);
+                  imageElement.style.maxWidth = '100%'; // Estilo para limitar a largura da imagem
+              }
+          });
       }
-    });
   }
-}
+
+  // Restante do seu código aqui...
+});
+
+// ... (outro código posterior)
+
 
 	// Adicione um ouvinte de evento para o botão "Contatos de Emergência"
   document.getElementById('mostrarContatosBtn').addEventListener('click', function () {
@@ -29,29 +46,41 @@ function acessarCamera() {
     }
   });
 
-  //function para localização
 
-  var askButton = document.getElementById('askButton');
-  var target = document.getElementById('target');
-  
-  function appendLocation(location) {
-    var newLocation = document.createElement('p');
-    newLocation.innerHTML = 'Latitude: ' + location.coords.latitude + ', Longitude: ' + location.coords.longitude;
-    target.appendChild(newLocation);
+// Função para salvar a localização do usuário
+function salvarLocalizacao() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const localizacao = `Latitude: ${latitude}, Longitude: ${longitude}`;
+
+      // Exiba a localização na página ou faça o que desejar com ela.
+      const targetElement = document.querySelector(".target");
+      targetElement.textContent = "Localização Salva: " + localizacao;
+
+      // Adicione a localização a um campo de formulário oculto
+      const localizacaoInput = document.createElement("input");
+      localizacaoInput.type = "hidden";
+      localizacaoInput.name = "localizacao";
+      localizacaoInput.value = localizacao;
+
+      // Anexe o campo de localização ao formulário
+      const form = document.querySelector("form");
+      form.appendChild(localizacaoInput);
+    });
+  } else {
+    alert("A geolocalização não é suportada neste navegador.");
   }
-  
-  askButton.addEventListener('click', function () {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(function (location) {
-        appendLocation(location);
-      }, function (error) {
-        console.error('Erro ao obter a localização: ' + error.message);
-      });
-    } else {
-      target.innerText = 'Geolocation API não suportada.';
-    }
-  });
-  
-  
+}
 
-  
+// Encontre o botão que acionará a função salvarLocalizacao
+const askButton = document.getElementById("askButton");
+
+// Adicione um ouvinte de evento de clique ao botão
+if (askButton) {
+  askButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita o envio do formulário
+    salvarLocalizacao(); // Chama a função para salvar a localização
+  });
+}
